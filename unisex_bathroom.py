5067616d85aca2735b944ee24425fbba9bf643e4
Rasengam1000 <pgcaninas@gmail.com>
2023-04-15 01:24:20 -0300

    final

--- unisex_bathroom.py
+++ unisex_bathroom.py
@@ -1,98 +1,102 @@
 import threading
-import time
-import random as r
-
-# Semáforos para controlar o acesso ao banheiro e a contagem de funcionários
-mutex = threading.Semaphore(1)
-
-banheiro = []  # Lista para representar o banheiro
-
-def main():
-            
-        # selecionar quant threads (= numero pessoas metade m metade f?)
-        # criar thread e elas tentam entrar, se n puder fica barreira
-        # t
+from time import sleep
+from random import randint
 
 
-    n_thread, tamanho_banheiro = [int(x) for x in input("Digite a quantidade de threads(pessoas) e o tamanho do banheiro: ").split(" ")]
+class funcionario:
+    def __init__(self, sexo):
+        self.sexo = sexo
+        self.iniciar()
 
-    for n in range(n_thread // 2):
-        t = threading.Thread(target=funcionario, args=('M',))
-        t.start() 
+    def iniciar(self):
 
-    for n in range(n_thread % 2 + n_thread // 2):
-        t = threading.Thread(target=funcionario, args=('F',))
-        t.start() 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-main()
-def usar_banheiro(sexo):
-    print(f'Funcionário do sexo {sexo} entrou no banheiro')
-    time.sleep(10)  # Simula o uso do banheiro por 2 segundos
-    if sexo == 'feminino':
-        banheiro.remove('F')
-    else:
-        banheiro.remove('M')
-    print(f'Funcionário do sexo {sexo} saiu do banheiro')
-
-def funcionario(sexo):
-    id_thread = threading.get_ident()
-
-    time.sleep(r.randint(0,10))
-    # lock lista banheiro
-    # checa se pode entrar, se n libera e entra barreira (ocupaçao do banheiro 0)
-    # threading.Barrier()
-    print(f"cliente {id_thread}, sexo {sexo} entrou no banheiro\n")
-
-
-
-def funcionario(sexo):
-    while True:
-        # Solicita ao usuário o próximo funcionário a entrar no banheiro
-
+        id_thread = threading.get_ident()
         
-        entrada = input("Digite o próximo funcionário a entrar no banheiro (M para homem, F para mulher): ").upper()
+        self.entrar_banheiro()
+        print(f"Cliente {self.sexo} {id_thread} ENTROU no banheiro\n")
 
-        # Verifica a escolha do usuário e incrementa a contagem de homens ou mulheres
-        if entrada == 'M':
-            mutex.acquire()
-            if 'F' not in banheiro and len(banheiro) < 3:  # Verifica se há menos de 3 homens no banheiro
-                banheiro.append('M')  # Adiciona 'M' à lista de funcionários no banheiro
-                mutex.release()
-                usar_banheiro('masculino')
-            else:
-                mutex.release()
-                print("Entrada de homem não permitida neste momento.")
-        elif entrada == 'F':
-            mutex.acquire()
-            if 'M' not in banheiro and len(banheiro) < 3:  # Verifica se há menos de 3 mulheres no banheiro
-                banheiro.append('F')  # Adiciona 'F' à lista de funcionários no banheiro
-                mutex.release()
-                usar_banheiro('feminino')
-            else:
-                mutex.release()
-                print("Entrada de mulher não permitida neste momento.")
+        sleep(randint(1,5))                         # "dorme"
+
+        self.sair_banheiro()
+        print(f"Cliente {self.sexo} {id_thread} SAIU do banheiro\n")
+
+
+    def entrar_banheiro(self):
+        if self.sexo == "M":
+            maximo_m.acquire()                      # impede mais de 3 entrarem
+            global m
+
+            mutexM.acquire()                        # mutex para acessar variavel que conta quantas pessoas tem
+
+            if m == 0:                              # se n tiver ninguem do mesmo sexo, tenta adquirir exclusividade do banheiro
+                ocupado.acquire()                   # (se outro sexo ja estiver dentro nao vai conseguir adquirir ate ele esvaziar) - semaforo binario
+                                                    
+          
+            m += 1                                  # + uma pessoa dentro
+
+            mutexM.release()
+
         else:
-            print("Entrada inválida! Por favor, digite 'M' para homem ou 'F' para mulher.")
+            maximo_f.acquire()                      # semaforo impede mais de 3 entrarem
+            global f                    
+            
+            mutexF.acquire()                        # mutex para acessar variavel que conta quantas pessoas tem
 
-# Criação das threads para os funcionários
-#for i in range(2):
-#    t = threading.Thread(target=funcionario, args=('M',))
-#    t.start()
+            if f == 0:                              # se n tiver ninguem do mesmo sexo, tenta adquirir exclusividade do banheiro
+                ocupado.acquire()                   # (se outro sexo ja estiver dentro nao vai conseguir adquirir ate ele esvaziar) - semaforo binario
+            
+            f += 1                                  # + uma pessoa dentro
 
-#for i in range(2):
-#    t = threading.Thread(target=funcionario, args=('F',))
-#    t.start() 
+            mutexF.release()
+
+    def sair_banheiro(self):
+        if self.sexo == "M":
+            maximo_m.release()                      # impede mais de 3 entrarem
+            global m
+
+            mutexM.acquire()                        # mutex para acessar variavel que conta quantas pessoas tem
+
+            m -= 1                                  # + uma pessoa dentro
+
+            if m == 0:
+                ocupado.release()                   # libera semaforo para que outro sexo pegue a exclusividade
+
+            mutexM.release()
+
+        else:
+            maximo_f.release()                      # impede mais de 3 entrarem
+            global f
+
+            mutexF.acquire()                        # mutex para acessar variavel que conta quantas pessoas tem
+            
+            f -= 1                                  # + uma pessoa dentro
+
+            if f == 0:
+                ocupado.release()                   # libera semaforo para que outro sexo pegue a exclusividade
+
+            mutexF.release()
+
+
+# n_thread, tamanho_banheiro = [int(x) for x in input("Digite o número de threads (pessoas) e o tamanho do banheiro").split()]
+n_thread = 10
+tamanho_banheiro = 3
+
+ocupado = threading.Semaphore(1)        # garante 1 sexo por vez
+
+m = 0
+f = 0
+
+mutexM = threading.Lock()
+mutexF = threading.Lock()
+
+maximo_m = threading.Semaphore(tamanho_banheiro)
+maximo_f = threading.Semaphore(tamanho_banheiro)
+
+
+for n in range(n_thread // 2):                              # metade
+    t = threading.Thread(target=funcionario, args=('M'))
+    t.start()
+
+for n in range(n_thread % 2 + n_thread // 2):               # metade ou metade + 1 para impar
+    t = threading.Thread(target=funcionario, args=('F'))
+    t.start()
